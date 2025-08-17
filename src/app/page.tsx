c432b8bfab7516e28213be7a99d767bc6fdd235e
Ubeyid Oumer <ubeyidah@gmail.com>
2025-08-17 20:59:46 +0300

    feat: setup shadcn ui and dark mode theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { ThemeSwitcher } from "@/components/theme-switcher";
+import { Button } from "@/components/ui/button";
 import {
   SignInButton,
   SignUpButton,
@@ -10,12 +12,13 @@ const page = () => {
   return (
     <div>
       <header className="flex justify-end items-center p-4 gap-4 h-16">
+        <ThemeSwitcher />
         <SignedOut>
-          <SignInButton mode="modal" />
+          <SignInButton mode="modal">
+            <Button variant={"secondary"}>Sign In</Button>
+          </SignInButton>
           <SignUpButton mode="modal">
-            <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-              Sign Up
-            </button>
+            <Button>Sign Up</Button>
           </SignUpButton>
         </SignedOut>
         <SignedIn>
